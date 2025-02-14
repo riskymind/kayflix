5e105bccbfc7e043b54d612748097973d9a856af
kelechi <oparaiskelechi@gmail.com>
2025-02-15 00:51:56 +0100

    setup the title cards

--- kayflix/src/components/TitleCard/TitleCards.jsx
+++ kayflix/src/components/TitleCard/TitleCards.jsx
@@ -1,11 +1,40 @@
-import '/TitleCards.css'
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
+import { useEffect, useRef } from "react";
+import PropTypes from 'prop-types';
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
   return (
-    <div className='titlecards'>
-      
+    <div className="title-cards">
+      <h2>{title?title:"Popular on Kayflix"}{category}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt={card.name} />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+TitleCards.propTypes = {
+  title: PropTypes.string, 
+  category: PropTypes.string
+};
+
+export default TitleCards;
